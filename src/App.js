a48c3298c2079f8d82fcd1e57e10390f435c5c76
Rory Thomson <rthomson700@gmail.com>
2022-11-11 22:41:12 +1300

    Changed power value handler. Now when power is switched the altitude is set to the correct value.
    Clean up and commenting.

--- src/App.js
+++ src/App.js
@@ -7,63 +7,70 @@ import needle from './assets/radaltneedle.png';
 import bugImg from './assets/bug.png';
 import flagImg from './assets/radaltflag.png';
 
-
-
 function App() {
   const [altitude, setAlt] = useState(0);
   const [bug, setBug] = useState(0);
   const [power, setPower] = useState(false);
-  console.log(power);
+
+  // While power can be handled inline, this handler ensures that the altitude dial is set
+  // to the correct value when power is switched back on.
+  function powerChange(event){
+    setPower(!power);
+    if(!power){
+      setAlt(document.getElementById("altitudeInput").value);
+    }
+  }
   
   return (
     <div class="capsule">
       <div class="altimeter">
         <img src={scale} class="scale" />
-        
         <AltitudeDial altitude = {altitude} power = {power}/>
         <BugDial bug = {bug} power = {power}/>
         <img src={cover} class="cover"/>
         <Light altitude = {altitude} bug ={bug} power = {power}/>
-        <Flag power = {power}/>     
+        <Flag power = {power}/>   
       </div>
       <div class="inputs">
         <div class="altInput">
-          <label for="altitude" type="range">Altitude</label>
-          <input type="range" min="0" max="1500" step="1" defaultValue="0" onChange={power ? (event) => setAlt(event.target.value): null} class="slider" />
-          <label for="bug" id="altitudeOutput">{altitude}</label>
+          Altitude
+          <input id="altitudeInput" type="range" min="0" max="1500" step="1" defaultValue="0" onChange={power ? (event) => setAlt(event.target.value): null} class="slider" />
+          {altitude}
         </div>
         <div class="bugInput">
-          <label for="bug" type="range"> Bug </label>
+          Bug
           <input type="range" min="0" max="1500" defaultValue='0' onChange={(event) => setBug(event.target.value)} class="slider" />
-          <label for="bug" id="bugOutput">{bug}</label>
+          {bug}
         </div>
         <div class="powerInput" type="checkbox">
-          <label  >Power</label>
-          <input type="checkbox" class="powerButton" value="true" onChange={(event) => setPower(!power)}/>
+          Power
+          <input type="checkbox" class="powerButton" value="false" onChange={powerChange} />  
         </div>
       </div>
     </div>
   );
 }
 
+//Determines the neccesary rotation for the altitude needle based on the value passed
 function AltitudeDial(props){
   var rotation;
-  //console.log(props.altitude);
   if(props.altitude <= 500){
+    //0.36 = 180/500
     rotation = 0.36 * props.altitude;
   }
   else{
+    //0.09 = 90/1000
     rotation = (0.09 * (props.altitude - 500)) +180;
   }
   let style = {
     transform: "rotate(" + rotation + "deg)"
   }
-  //console.log(rotation);
   return(
     <img src={needle} class="needle" style={style}></img>
   );
 }
 
+//Same as AltitudeDial but for the DH bug
 function BugDial(props){
   var rotation;
   if(props.bug <= 500){
@@ -75,12 +82,12 @@ function BugDial(props){
   let style = {
     transform: "rotate(" + rotation + "deg)"
   }
-  //console.log(rotation);
   return(
     <img src={bugImg} class="bug" style={style}></img>
   );
 }
 
+//Shows indicator light when power is on and altitude is lower than the bug
 function Light(props){
  if((props.bug - props.altitude >0) && props.power){
   return(<span class="lightOn"/>)
@@ -88,6 +95,7 @@ function Light(props){
   return(<span class="lightOff"></span>)
 }
 
+//Hides the radalt flag if power is on
 function Flag(props){
   if(props.power){
     return <img hidden="hidden" src={flagImg} class="flag"/>   
@@ -96,7 +104,5 @@ function Flag(props){
     return <img src={flagImg} class="flag"/>   
   }
 }
-
-
 
 export default App;
