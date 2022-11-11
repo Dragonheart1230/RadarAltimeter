993edae70ffe84a7a5bdd862387ffed4c0d620ad
Rory Thomson <rthomson700@gmail.com>
2022-11-11 17:54:39 +1300

    Refactored code. Implemented dials with hooks.

--- src/App.js
+++ src/App.js
@@ -7,37 +7,86 @@ import needle from './assets/radaltneedle.png';
 import bugImg from './assets/bug.png';
 import flagImg from './assets/radaltflag.png';
 
+
+
 function App() {
+  const [altitude, setAlt] = useState(0);
+  const [bug, setBug] = useState(0);
+
 
   return (
     <div class="capsule">
       <div class="altimeter">
-        <img src={scale} class="scale"></img>
-        <img src={cover} class="cover"></img>
-        <img src={needle} class="needle"></img>
-        <img src={bugImg} class="bug"></img>
-        <img src={flagImg} class="flag"></img>
-        <span class="lightOff"></span>
+        <img src={scale} class="scale" />
+        <img src={cover} class="cover"/>
+        <AltitudeDial altitude = {altitude}/>
+        <BugDial bug = {bug}/>
+        <Light/>
+        <img src={flagImg} class="flag"/>     
       </div>
       <div class="inputs">
         <div class="altInput">
           <label for="altitude" type="range">Altitude</label>
-          <input type="range" min="0" max="1500" defaultValue={0} class="slider" id="altitude"/>
-          <label for="bug" id="altitudeOutput">0</label>
+          <input type="range" min="0" max="1500" defaultValue="0" onChange={(event) => setAlt(event.target.value)} class="slider" />
+          <label for="bug" id="altitudeOutput">{altitude}</label>
         </div>
-        <div class="budInput">
+        <div class="bugInput">
           <label for="bug" type="range"> Bug </label>
-          <input type="range" min="00" max="1500" defaultValue={0} class="slider" id="bug"/>
-          <label for="bug" id="bugOutput">0</label>
+          <input type="range" min="0" max="1500" defaultValue='0' onChange={(event) => setBug(event.target.value)} class="slider" />
+          <label for="bug" id="bugOutput">{bug}</label>
         </div>
         <div class="powerInput" type="checkbox">
           <label for="power" >Power</label>
-          <input type="checkbox" class="powerButton" id="power"/>
+          <input type="checkbox" class="powerButton" id="power" checked='false' />
         </div>
       </div>
     </div>
   );
 }
+
+function AltitudeDial(props){
+  var rotation;
+  console.log(props.altitude);
+  if(props.altitude <= 500){
+    rotation = 0.36 * props.altitude;
+  }
+  else{
+    rotation = (0.09 * (props.altitude - 500)) +180;
+  }
+  let style = {
+    transform: "rotate(" + rotation + "deg)"
+  }
+  console.log(rotation);
+  return(
+    <img src={needle} class="needle" style={style}></img>
+  );
+}
+
+function BugDial(props){
+  var rotation;
+  if(props.bug <= 500){
+    rotation = 0.36 * props.bug;
+  }
+  else{
+    rotation = (0.09 * (props.bug - 500)) +180;
+  }
+  let style = {
+    transform: "rotate(" + rotation + "deg)"
+  }
+  console.log(rotation);
+  return(
+    <img src={bugImg} class="bug" style={style}></img>
+  );
+}
+
+function Light(props){
+  var [power, setPower] = useState(false);
+  return(
+    <span class="lightOff"></span>
+  );
+}
+
+
 
 
 export default App;
